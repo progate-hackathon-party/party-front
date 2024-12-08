488ab5feb660c954f605aa3134171d3461f7017a
0P01003 <kd1370025@st.kobedenshi.ac.jp>
2024-12-08 13:08:14 +0900

    メイン画面をExpoで動くように移植

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,20 @@
-import "expo-router/entry";
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import {router} from "expo-router";
 import {
     SafeAreaView,
     ScrollView,
     StatusBar,
     StyleSheet,
+    Text,
     useColorScheme,
     View,
+    Dimensions
 } from 'react-native';
 import { Pressable, Image } from "react-native";
-import "maplibre-gl/dist/maplibre-gl.css";
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import {router} from "expo-router";
-import Mapbox, {Camera, MapView, PointAnnotation} from "@rnmapbox/maps";
-import Geolocation, {GeolocationResponse} from '@react-native-community/geolocation';
-import {RegionPayload} from "@rnmapbox/maps/src/components/MapView";
 
-Mapbox.setAccessToken("pk.eyJ1IjoibW9ub2thbW8iLCJhIjoiY200ZWozbjFhMGJjdTJrb3VmMHk4ejhscSJ9.SaCvoIQ_-wXSpTI2oDsm6A");
-
-function HomePage() {
-    const isDarkMode = useColorScheme() === 'dark';
+function HomePage({ navigation }: { navigation: any }) {
+    const isDarkMode = useColorScheme() !== 'dark';
     const main_styles = StyleSheet.create({
         container: {
             flex: 1,
@@ -29,115 +24,92 @@ function HomePage() {
             flexGrow: 1,
         },
         centerContainer: {
-            justifyContent: 'center',
+            justifyContent: 'flex-start',  // テキストを上に配置
             alignItems: 'center',
             flex: 1,
+            paddingTop: 20,  // 上部に少し余白を追加
+
         },
-        map: {
-            flex: 1
-        }
+        box: {
+            width: Dimensions.get('window').width *0.88,  // ボックスの幅
+            height: Dimensions.get('window').height *0.45, // ボックスの高さ
+            backgroundColor: '#ffffff77', // 白い背景
+            justifyContent: 'flex-start', // 内容を中央揃え
+            alignItems: 'center', // 内容を中央揃え
+            borderRadius: 10, // 角を丸める
+            shadowColor: '#000', // シャドウの色
+            shadowOffset: { width: 0, height: 1 }, // シャドウの位置
+            shadowOpacity: 0.1, // シャドウの濃さ
+            shadowRadius: 5, // シャドウのぼかし具合
+            padding:10
+        },
     });
 
-    const [location, setLocation] = useState<GeolocationResponse>();
-
-    useEffect(() => {
-        Geolocation.getCurrentPosition(
-            position => setLocation(position),
-            err => alert(err.message),
-            { enableHighAccuracy: true, timeout: 3000000, maximumAge: 10000 },
-        );
-    }, []);
-
-    const [visibleBounds, setVisibleBounds] = useState<Array<GeoJSON.Position>>();
-
-    // ユーザーの視界が変わった時に座標を取得する
-    const handleRegionChange = async (event:GeoJSON.Feature<GeoJSON.Point, RegionPayload>) => {
-        const { visibleBounds } = event.properties;
-
-        try{
-            const result = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/posts/location`,{
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    lat: [visibleBounds[1][1], visibleBounds[0][1]],
-                    lon: [visibleBounds[1][0], visibleBounds[0][0]],
-                }),
-            })
-
-            const data = await result.json();
-            let posts:Array<GeoJSON.Position> = []
-
-            data.posts.map((post:any) => {
-                const position:GeoJSON.Position = [post.attribute_values.lon, post.attribute_values.lat]
-                posts.push(position)
-            })
-
-            setVisibleBounds(posts);
-        }catch (e){
-            console.log(e)
-        }
-    };
-
     return (
-        <>
-        {location && location.coords
-            ?<SafeAreaView style={main_styles.container}>
-                <StatusBar
-                    barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                    backgroundColor={main_styles.container.backgroundColor}
-                />
-                <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={main_styles.scrollView}>
-                    <MapView style={main_styles.map}
-                         onRegionDidChange={(event) => handleRegionChange(event)}
-                    >
-                        <Camera
-                            defaultSettings={{ centerCoordinate: [location.coords.longitude, location.coords.latitude], zoomLevel: 15 }}
-                        />
-                        {visibleBounds
-                            ?visibleBounds.map((position, index) => {
-                                return (
-                                    <PointAnnotation
-                                        id={`pointAnnotation${index}`}
-                                        coordinate={position}
-                                    >
-                                        <View style={styles.pinContainer}>
-                                            <Image
-                                                source={require("../assets/images/map_icon.png")} // ローカル画像を指定
-                                                style={styles.image}
-                                            />
-                                        </View>
-                                    </PointAnnotation>
-                                );
-                            })
-                            :<SafeAreaView/>
-                        }
-
-                    </MapView>
-                </ScrollView>
-                {/* 右下に配置される要素 */}
-                <View style={styles.container}>
-                    <Pressable
-                        style={styles.button}
-                        onPress={() => {
-                            router.push("/details")
-                        }}
-                    >
-                        <Image
-                            source={require("../assets/images/map_icon.png")} // ローカル画像を指定
-                            style={styles.image}
-                        />
-                    </Pressable>
+        <SafeAreaView style={main_styles.container}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={main_styles.container.backgroundColor}
+            />
+            <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={main_styles.scrollView}>
+                <View style={{ flexDirection: 'row', marginTop: 40 }}>
+                    <Text style={[isDarkMode ? {color:"#ffffff"} : {color:"#000000"}, {fontSize: 30, paddingLeft: 25, fontWeight: 'bold'}]}>
+                        近くのセール
+                    </Text>
                 </View>
-            </SafeAreaView>
-            :<SafeAreaView/>
-        }
-        </>
+
+                <View style={main_styles.centerContainer}>
+                    {/* 四角いボックス */}
+                    <View style={main_styles.box}>
+                        <View style={{ paddingLeft: 60, flexDirection: 'row', alignItems: 'flex-start', paddingRight: 80, padding: 10 }}>
+                            <Image
+                                source={require("../assets/images/map_icon.png")}
+                                style={[styles.image, { marginTop: 10, marginRight: 15 }]} // 横並び用のスタイル
+                            />
+                            <View style={{ alignItems: 'flex-start' }}> {/* ここを変更 */}
+                                <Text style={{ fontSize: 25, fontWeight: 'bold', color: isDarkMode ? '#ffffff' : '#000000' }}>
+                                    aaa店
+                                </Text>
+                                <Text style={{ fontSize: 10, fontWeight: 'bold', color: isDarkMode ? '#ffffff' : '#000000' }}
+                                      numberOfLines={3}  // 1行に制限
+                                      ellipsizeMode="tail"  // 文字数を超えた場合に「...」を追加
+                                >
+                                    📍 ラーメン一郎店 🍜 特製味噌ラーメンが500円引き！ 📅 セール期間: 12月10日〜12月20日 ⏰ 営業時間: 11:00〜22:00 ✨ 注目ポイント: セール期間中はトッピング1品無料！
+                                </Text>
+                            </View>
+                            <Text style={{ paddingLeft: 18, paddingTop: 47, fontSize: 18, fontWeight: 'bold', color: isDarkMode ? '#ffffff' : '#000000' }}>8m</Text>
+                        </View>
+                        <Text style={{ fontSize: 7}}>_________________________________________________________________________</Text>
+                    </View>
+
+                </View>
+
+            </ScrollView>
+            {/* 右下に配置される要素 */}
+            <View style={styles.container}>
+                <Pressable
+                    style={styles.button}
+                    onPress={() => {
+                        router.push("/map")
+                    }}
+                >
+                    <Image
+                        source={require("../assets/images/map_icon.png")} // ローカル画像を指定
+                        style={styles.image}
+                    />
+                </Pressable>
+            </View>
+        </SafeAreaView>
     );
 }
+const insertLineBreaks = (text: string, maxLength: number): string => {
+    const regex = new RegExp(`(.{1,${maxLength}})`, 'g'); // 最大 maxLength ごとに分割
+    return text.match(regex)?.join('\n') ?? ""; // 分割した文字列を改行で結合
+};
 
 const styles = StyleSheet.create({
+
+
     container: {
         position: "absolute", // 絶対位置を設定
         bottom: 20, // 下から20px
@@ -148,14 +120,11 @@ const styles = StyleSheet.create({
         borderRadius: 50,
         padding: 15,
     },
+
     image: {
         width: 50, // 画像の幅
         height: 50, // 画像の高さ
         resizeMode: "contain", // 画像の比率を維持して縮小/拡大
-    },
-    pinContainer: {
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 });
 
---
+++ app/map/index.tsx
@@ -0,0 +1,162 @@
+import "expo-router/entry";
+import React, {useEffect, useState} from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    useColorScheme,
+    View,
+} from 'react-native';
+import { Pressable, Image } from "react-native";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import {router} from "expo-router";
+import Mapbox, {Camera, MapView, PointAnnotation} from "@rnmapbox/maps";
+import Geolocation, {GeolocationResponse} from '@react-native-community/geolocation';
+import {RegionPayload} from "@rnmapbox/maps/src/components/MapView";
+
+Mapbox.setAccessToken("pk.eyJ1IjoibW9ub2thbW8iLCJhIjoiY200ZWozbjFhMGJjdTJrb3VmMHk4ejhscSJ9.SaCvoIQ_-wXSpTI2oDsm6A");
+
+function HomePage() {
+    const isDarkMode = useColorScheme() === 'dark';
+    const main_styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        },
+        scrollView: {
+            flexGrow: 1,
+        },
+        centerContainer: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            flex: 1,
+        },
+        map: {
+            flex: 1
+        }
+    });
+
+    const [location, setLocation] = useState<GeolocationResponse>();
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(
+            position => setLocation(position),
+            err => alert(err.message),
+            { enableHighAccuracy: true, timeout: 3000000, maximumAge: 10000 },
+        );
+    }, []);
+
+    const [visibleBounds, setVisibleBounds] = useState<Array<GeoJSON.Position>>();
+
+    // ユーザーの視界が変わった時に座標を取得する
+    const handleRegionChange = async (event:GeoJSON.Feature<GeoJSON.Point, RegionPayload>) => {
+        const { visibleBounds } = event.properties;
+
+        try{
+            const result = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/posts/location`,{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    lat: [visibleBounds[1][1], visibleBounds[0][1]],
+                    lon: [visibleBounds[1][0], visibleBounds[0][0]],
+                }),
+            })
+
+            const data = await result.json();
+            let posts:Array<GeoJSON.Position> = []
+
+            data.posts.map((post:any) => {
+                const position:GeoJSON.Position = [post.attribute_values.lon, post.attribute_values.lat]
+                posts.push(position)
+            })
+
+            setVisibleBounds(posts);
+        }catch (e){
+            console.log(e)
+        }
+    };
+
+    return (
+        <>
+        {location && location.coords
+            ?<SafeAreaView style={main_styles.container}>
+                <StatusBar
+                    barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                    backgroundColor={main_styles.container.backgroundColor}
+                />
+                <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={main_styles.scrollView}>
+                    <MapView style={main_styles.map}
+                         onRegionDidChange={(event) => handleRegionChange(event)}
+                    >
+                        <Camera
+                            defaultSettings={{ centerCoordinate: [location.coords.longitude, location.coords.latitude], zoomLevel: 15 }}
+                        />
+                        {visibleBounds
+                            ?visibleBounds.map((position, index) => {
+                                return (
+                                    <PointAnnotation
+                                        id={`pointAnnotation${index}`}
+                                        coordinate={position}
+                                    >
+                                        <View style={styles.pinContainer}>
+                                            <Image
+                                                source={require("../../assets/images/map_icon.png")} // ローカル画像を指定
+                                                style={styles.image}
+                                            />
+                                        </View>
+                                    </PointAnnotation>
+                                );
+                            })
+                            :<SafeAreaView/>
+                        }
+
+                    </MapView>
+                </ScrollView>
+                {/* 右下に配置される要素 */}
+                <View style={styles.container}>
+                    <Pressable
+                        style={styles.button}
+                        onPress={() => {
+                            router.push("/post")
+                        }}
+                    >
+                        <Image
+                            source={require("../../assets/images/map_icon.png")} // ローカル画像を指定
+                            style={styles.image}
+                        />
+                    </Pressable>
+                </View>
+            </SafeAreaView>
+            :<SafeAreaView/>
+        }
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: "absolute", // 絶対位置を設定
+        bottom: 20, // 下から20px
+        right: 20, // 右から20px
+    },
+    button: {
+        backgroundColor: "#D4D4FF",
+        borderRadius: 50,
+        padding: 15,
+    },
+    image: {
+        width: 50, // 画像の幅
+        height: 50, // 画像の高さ
+        resizeMode: "contain", // 画像の比率を維持して縮小/拡大
+    },
+    pinContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default HomePage;
